6012e6e5dee32d6e52ff66ea6c1321b8e427abb8
amine <mesbahminos@gmail.com>
2024-08-28 00:28:19 +0100

    create the first number and its display on the calculator

--- javascript.js
+++ javascript.js
@@ -1,3 +1,6 @@
+let firstNumber = "";
+let operator = "";
+
 function add(a, b) {
   return a + b;
 }
@@ -10,3 +13,16 @@ function multiply(a, b) {
 function divide(a, b) {
   return a / b;
 }
+const display = document.querySelector(".display");
+const number = document.querySelectorAll(".number");
+
+number.forEach((item) =>
+  item.addEventListener("click", () => {
+    const customIndex = item.dataset.index;
+    if (operator == "" && firstNumber.length < 30) {
+      firstNumber = firstNumber + `${customIndex}`;
+      display.textContent = firstNumber;
+      console.log(firstNumber);
+    }
+  })
+);
