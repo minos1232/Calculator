7a06b8c06c4b9737e40e3b60202a5fa962b549e0
amine <mesbahminos@gmail.com>
2024-08-28 20:37:40 +0100

    add the operator and negation of the first number

--- javascript.js
+++ javascript.js
@@ -15,14 +15,33 @@ function divide(a, b) {
 }
 const display = document.querySelector(".display");
 const number = document.querySelectorAll(".number");
+const signs = document.querySelectorAll(".operator");
+const negate = document.querySelector(".negation");
 
 number.forEach((item) =>
   item.addEventListener("click", () => {
-    const customIndex = item.dataset.index;
+    const numbersIndex = item.dataset.index;
     if (operator == "" && firstNumber.length < 30) {
-      firstNumber = firstNumber + `${customIndex}`;
+      firstNumber = firstNumber + `${numbersIndex}`;
       display.textContent = firstNumber;
       console.log(firstNumber);
     }
   })
 );
+signs.forEach((sign) => {
+  sign.addEventListener("click", () => {
+    const signsIndex = sign.dataset.index;
+    if ((firstNumber = !"")) operator = `${signsIndex}`;
+    console.log(operator);
+  });
+});
+
+negate.addEventListener("click", () => {
+  if (firstNumber == "") {
+    firstNumber = "-";
+    display.textContent = firstNumber;
+  } else if ((firstNumber = !"")) {
+    operator = "-";
+    console.log(operator);
+  }
+});
